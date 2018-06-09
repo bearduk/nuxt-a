ea36e4dd51ed9502c73baa6e3f2170005e12f285
Chris Beard <coebe500@aol.com>
2018-06-09 09:17:59 +0100

    Component Header. Added to Layouts - default and users.

--- layouts/users.vue
+++ layouts/users.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
-      <header><h1>Users header from Layout users.vue</h1></header>
+    <Header /> <!-- imported Component -->
+    <p> This header is coming from the Header component which is imported into the users.uve Layout</p>
     <nuxt/>
   </div>
 </template>
+
+<script>
+import Header from '~/components/Header.vue'
+
+export default {
+  components: {
+   Header
+  }
+}
+</script>
 
 <style>
 header {
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
   <section class="container">
-
     <a href="/users">Normal hard link to /users route</a>
     <p>...and here is the nuxt link that won't reload the page - </p>
     <nuxt-link to="/users"> Nuxt-link to /users</nuxt-link>
@@ -9,43 +8,8 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
-export default {
-  components: {
-    Logo
-  }
-}
 </script>
 
 <style>
-.container
-{
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.title
-{
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-.subtitle
-{
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-.links
-{
-  padding-top: 15px;
-}
 </style>
