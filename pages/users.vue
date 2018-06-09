b2af876cfee3d3e156c46cbde5695696ea758034
Chris Beard <coebe500@aol.com>
2018-06-09 08:28:56 +0100

    Nested routes example

---
+++ pages/users.vue
@@ -0,0 +1,26 @@
+<template>
+        <div>
+        <nuxt-link to="/">Home</nuxt-link>
+        <input type="text" v-model="userId">
+        <button @click="onLoadUser">Load user</button>
+        <p>The above will push() the input to the $router that is accepted as the id in _id folder</p>
+
+        <nuxt-child />
+    </div>
+</template>
+
+
+<script>
+    export default {
+       data(){
+           return {
+               userId: ''
+           }
+       } ,
+       methods: {
+           onLoadUser(){
+               this.$router.push('/users/' + this.userId)
+           }
+       }
+    }
+</script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,26 +1,7 @@
 <template>
-    <div>
-        <nuxt-link to="/">Home</nuxt-link>
-        <input type="text" v-model="userId">
-        <button @click="onLoadUser">Load user</button>
-        <p>The above will push() the input to the $router that is accepted as the id in _id folder</p>
-    </div>
+<p>Please enter an id and enter</p>
 </template>
 
-<script>
-    export default {
-       data(){
-           return {
-               userId: ''
-           }
-       } ,
-       methods: {
-           onLoadUser(){
-               this.$router.push('/users/' + this.userId)
-           }
-       }
-    }
-</script>
 
 <style scoped>
 
