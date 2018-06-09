c0337fc9fe0293e4a9280f4b78185c9f0c4420d3
Chris Beard <coebe500@aol.com>
2018-06-09 09:01:00 +0100

    built in validate method now checks user id is an integer

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,3 +1,6 @@
 <template>
-    <h1>Sorry an error occurred, oh no!</h1>
+    <div>
+        <h1>Sorry an error occurred, oh no!</h1>
+        <p>If you're viewing a user, did you enter an integer?</p>
+    </div>    
 </template>
--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -7,7 +7,10 @@
 
 <script>
     export default {
-        
+        validate (data){
+            console.log(data);
+            return /^\d+$/.test(data.params.id)
+        } 
     }
 </script>
 
